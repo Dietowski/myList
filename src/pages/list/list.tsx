32d1f01a497661816fe2357a7e2a6e6ba8c6e35e
Diego Felipe Pereira <dfelipe@rs.local>
2026-04-01 17:28:14 -0300

    second steps

--- src/pages/list/list.tsx
+++ src/pages/list/list.tsx
@@ -1,43 +1,77 @@
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { themes } from '../../global/themes';
 import './listStyles';
 import { style } from './listStyles';
+import { useState } from 'react';
 
 export default function Lists() {
+
+    const [pesquisa, setPesquisa] = useState('');
 
     return(
         <View style={style.container}>
             <View style={style.rowInitial}>  {/* ROW INICIAL */}
-                <MaterialIcons 
-                    name='menu'
-                    size={30}
-                    color={themes.colors.gray}
-                />
+                <TouchableOpacity>
+                    <MaterialIcons 
+                        name='menu'
+                        size={30}
+                        color={themes.colors.gray}
+                    />
+                </TouchableOpacity>
                 <Text style={style.textRowInitial}>Mouseion</Text>
                 <View style={style.rightIcons}>
-                    <MaterialIcons
-                        name='notifications-none'
-                        size={30}
-                        color={themes.colors.gray}
-                    />
-                    <MaterialIcons 
-                        name='account-circle'
-                        size={30}
-                        color={themes.colors.gray}
-                    />
+                    <TouchableOpacity>
+                        <MaterialIcons
+                            name='notifications-none'
+                            size={30}
+                            color={themes.colors.gray}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <MaterialIcons 
+                            name='account-circle'
+                            size={30}
+                            color={themes.colors.gray}
+                        />
+                    </TouchableOpacity>
                 </View>
             </View>
-            <View>  {/* ROW CUMPRIMENTOS */}
-                <Text>Olá usuário, qual livro vai ler hoje?</Text>
+            <View style={{alignItems:'center', justifyContent: 'center', paddingVertical: 31}}>  {/* ROW CUMPRIMENTOS */}
+                <Text style={{fontSize:20}}>Olá usuário, qual livro vai ler hoje?</Text>
             </View>
-            <View style={style.rowInput}>  {/* ROW BARRA DE PESQUISA */}
-                <TextInput 
-                    placeholder='Livro'
-                />
+            <View style={style.boxRowInput}>
+                <View style={style.rowInput}>  {/* ROW BARRA DE PESQUISA */}
+                    <TextInput
+                        placeholder='Livro'
+                        placeholderTextColor={themes.colors.gray}
+                        value={pesquisa}
+                        onChangeText={setPesquisa}
+                    />
+                    <TouchableOpacity onPress={() => setPesquisa('')}>
+                        <MaterialIcons 
+                            name='clear'
+                            size={16}
+                            color={themes.colors.gray}
+                        />
+                    </TouchableOpacity>
+                </View>
             </View>
             <View>  {/* ROW CATEGORIA 1 */}
-
+                <View style={style.rowCategoriaNome}>
+                    <Text style={{fontSize:24}}>Matemática</Text>
+                    <TouchableOpacity>
+                        <MaterialIcons 
+                            name='arrow-forward-ios'
+                            size={16}
+                            color={themes.colors.gray}
+                            style={{paddingVertical: 10}}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={style.containerLivros}> {/* ROW CARD LIVROS */}
+                <Text>AA</Text>
             </View>
         </View>
     );
--- src/pages/list/listStyles.ts
+++ src/pages/list/listStyles.ts
@@ -10,7 +10,8 @@ export const style = StyleSheet.create({
         flexDirection: 'row',
         marginTop: 20,
         justifyContent: 'space-between',
-        padding: 32,
+        paddingTop: 32,
+        paddingHorizontal: 10,
         alignItems: 'center'
     },
     textRowInitial: {
@@ -21,8 +22,30 @@ export const style = StyleSheet.create({
         flexDirection: 'row',
         gap: 16
     },
+    boxRowInput: {
+        alignItems: 'center',
+    },
     rowInput: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 16,
+        alignItems: 'center',
+        width: 239,
+        height: 40,
         backgroundColor: themes.colors.primary,
-        
+        borderRadius: 50,
+    },
+    rowCategoriaNome: {
+        paddingTop: 31,
+        flexDirection: 'row',
+        gap: 5,
+        paddingHorizontal: 10,
+    },
+    containerLivros: {
+        backgroundColor: themes.colors.lightGray,
+        borderRadius: 15,
+        height: 165,
+        width: 340,
+        alignSelf: 'center',
     }
 });
